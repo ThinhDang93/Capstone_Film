9e0cdc3a5308536154e0e49825ef1436c528ec04
Thinhdang <thinhdang@Thinhdangs-MacBook-Pro.local>
2025-08-18 22:19:37 +0700

    first commit

---
+++ src/Pages/Admin/Ad_FileManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ad_FileManagement = () => {
+  return (
+    <div>Ad_FileManagement</div>
+  )
+}
+
+export default Ad_FileManagement
---
+++ src/Pages/Admin/Ad_UserManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ad_UserManagement = () => {
+  return (
+    <div>Ad_UserManagement</div>
+  )
+}
+
+export default Ad_UserManagement
